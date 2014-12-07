2b94628ac8800184b8f8a1ad88638893b8c1926e
Mani bhushan <manibhushan.cs@gmail.com>
2014-12-07 05:52:21 +0530

    plotting projectile related data

---
+++ chap15/projectiles/projectile.py
@@ -0,0 +1,55 @@
+import pylab
+
+
+def getTrajectory(filename):
+    f = open(filename, 'r')
+    distances = []
+    h1, h2, h3, h4 = [], [], [], []
+    f.readline()
+    for line in f:
+        d, ah, bh, ch, dh = line.split(",")
+        distances.append(float(d))
+        h1.append(float(ah))
+        h2.append(float(bh))
+        h3.append(float(ch))
+        h4.append(float(dh))
+    f.close()
+    return (distances, [h1, h2, h3, h4])
+
+
+def processTrajectories(filename):
+    dist, heights = getTrajectory(filename)
+    trials = len(heights)
+    dist = pylab.array(dist)
+
+    # get array combiming mean height at each distances
+    totHeights = pylab.array([0] * len(dist))
+    for h in heights:
+        totHeights = totHeights + pylab.array(h)
+    meanHeights = totHeights/len(heights)
+
+    # start plotting :)
+    pylab.title("Trajectile of Projectile " + "(Mean of " + str(trials)
+                + " trails)")
+    pylab.xlabel("Inches from launch point")
+    pylab.ylabel("Inches above launch point")
+    pylab.plot(dist, meanHeights, 'bo')
+
+    a, b = pylab.polyfit(dist, meanHeights, 1)
+    altitudes = a * dist + b
+    pylab.plot(dist, altitudes, 'b', label='linear fit')
+
+    a, b, c = pylab.polyfit(dist, meanHeights, 2)
+    altitudes = a*(dist**2) + b*(dist) + c
+    pylab.plot(dist, altitudes, 'b:', label="Quadratic Fit")
+    pylab.legend()
+
+    pylab.show()
+
+
+def main():
+    filename = "projdata.csv"
+    processTrajectories(filename)
+
+if __name__ == '__main__':
+    main()
