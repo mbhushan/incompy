8a270fea301315fd349cd7c27e948ad8f53c34bf
Mani bhushan <manibhushan.cs@gmail.com>
2014-12-15 07:41:21 +0530

    adding graph class where graph inherits from directedGraph

---
+++ chap17/graphs/digraph.py
@@ -0,0 +1,45 @@
+from node_edge import Edge
+
+
+class Digraph(object):
+    # nodes is a list of the nodes in the graph
+    # edges is a dict mapping each node to a list of its children
+    def __init__(self):
+        self.nodes = []
+        self.edges = {}
+
+    def addNode(self, node):
+        if node in self.nodes:
+            raise ValueError("Duplicate Node")
+        else:
+            self.nodes.append(node)
+            self.edges[node] = []
+
+    def addEdge(self, edge):
+        src = edge.getSource()
+        dst = edge.getDestination()
+        if not(src in self.nodes and dst in self.nodes):
+            raise ValueError("Nodes not in graph")
+        self.nodes[src].append(dst)
+
+    def childrenOf(self, node):
+        return self.edges[node]
+
+    def hasNode(self, node):
+        return node in self.nodes
+
+    def __str__(self):
+        result = ''
+        for src in self.nodes:
+            for dst in self.edges[src]:
+                result += src.getName() + "->" + dst.getName() + "\n"
+        # omit final new line
+        return result[:-1]
+
+
+class Graph(Digraph):
+
+    def addEdge(self, edge):
+        Digraph.addEdge(self, edge)
+        rev = Edge(edge.getDestination(), edge.getSource())
+        Digraph.addEdge(self, rev)
